a16b461e2115ccbf92c24a34a31206ee389bf744
Haris <harisgujjar317@gmail.com>
2024-03-20 13:24:42 -0400

    adding mongoose Validation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const number = require('joi/lib/types/number');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
@@ -5,28 +6,59 @@ mongoose.connect('mongodb://localhost/playground')
     .catch(err => console.error('Database Connection Failed:', err));
 
 const courseSchema = new mongoose.Schema({
-    name: String,
+    // validation
+    name: { type: String, required: true },
+    category: {
+        type: String,
+        required: true,
+        // enum will check if the value is presant in the array 
+        enum: ['FrontEnd', 'BackEnd', 'DevOps']
+    },
     author: String,
-    tags: [String],
+    tags: {
+        type: Array, 
+        validate: {
+            validator: async function (v) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        const result = v && v.length > 0;
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(new Error("Course Length Should be greater than 1"))
+                        }
+                        
+                    }, 4000);
+                })
+            },
+            message: "Course Length Should be greater than 1"
+        }
+    },
     isPublished: Boolean,
     date: { type: Date, default: Date.now },
+    price: { type: Number, required: function () { return this.isPublished; } }
 });
 
 const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
+        // if we try to create course without name then it will not create the course and reject the promise
         name: 'Mongo DB',
-        author: 'haris',
-        tags: ['Mongo', 'Database'],
+        //category: 'FrontEnd',
+        author: 'hamza',
+        //tags: [], //"Backend", "Django"
         isPublished: true,
+        price: 12,
     });
 
     try {
         const result = await course.save();
         console.log('Course created:', result);
-    } catch (error) {
-        console.error('Error creating course:', error);
+        // handling errors like pro
+    } catch (err) {
+        for (field in err.errors)
+            console.error('Error creating course:', err.errors[field].message);
     }
 }
 
@@ -40,28 +72,5 @@ async function getCourses() {
     }
 }
 
-// createCourse();
+createCourse();
 // getCourses();
-
-// //updating the Document
-async function updateDocument(id) {
-    try {
-
-        const course = await Course.findById(id);
-        if (!course) {
-            console.log(course)
-            console.log('Course not found');
-            return;
-        }
-        
-        course.author = 'Hamza'; // Modify the 'author' property directly
-
-        // Save the modified document to the database
-        const result = await course.save();
-        console.log('Document updated successfully:', result);
-    } catch (err) {
-        console.error('Error updating document:', err);
-    }
-}
-
-updateDocument('65f8b0f95bf327e31a6c0cfd');
